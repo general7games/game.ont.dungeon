ecc3086be9019a269bd725ceb2ab8ee0335f108b
bluesky139 <bluesky139@gmail.com>
2018-11-28 16:04:14 +0800

    Pixel art drag and zoom.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -4,6 +4,28 @@ import { MapService, Point } from '../map.service'
 import { Observable } from 'rxjs';
 import * as utils from '../utils'
 
+enum Mode {
+	View = 0,
+	Draw,
+	Erase
+}
+
+interface XY {
+	x: number
+	y: number
+}
+
+const FLAG_MOUSE_MOVE  = 0x01
+const FLAG_MOUSE_WHEEL = 0x02
+const FLAG_MOUSE_DOWN  = 0x04
+const FLAG_MOUSE_UP    = 0x08
+const FLAG_MOUSE_DRAG  = 0x10
+
+const FLAGS_MODE = {
+	0/* Mode.View */: FLAG_MOUSE_MOVE | FLAG_MOUSE_WHEEL | FLAG_MOUSE_DRAG | FLAG_MOUSE_DOWN | FLAG_MOUSE_UP,
+	1/* Mode.Draw */: FLAG_MOUSE_MOVE | FLAG_MOUSE_WHEEL | FLAG_MOUSE_UP
+}
+
 @Component({
 	selector: 'app-map',
 	templateUrl: './map.component.html',
@@ -14,9 +36,20 @@ export class MapComponent implements OnInit {
 
 	context: CanvasRenderingContext2D
 	bubble: CanvasRenderingContext2D
-	scale: number = 6
+	
+	art: Array<string>
+	palette
+	scale = 6
+	lastScaleTime = 0
+
 	maxLine: number
 	points: Array<Point>
+
+	mode: Mode = Mode.View
+	lastDragXY: XY
+	totalOffset: XY = {
+		x: 0, y: 0
+	}
 
 	constructor(
 		private mapService: MapService,
@@ -40,22 +73,47 @@ export class MapComponent implements OnInit {
 
 		this.fillImageData().subscribe((filled) => {
 			if (filled) {
+				let isMouseDown = false
 				bubbleCanvas.addEventListener('mousemove', (ev: MouseEvent) => {
-					this.onMouseMove(ev)
+					if ((FLAGS_MODE[this.mode] & FLAG_MOUSE_DRAG) && isMouseDown) {
+						this.onMouseDrag(ev)
+					} else if ((FLAGS_MODE[this.mode] & FLAG_MOUSE_MOVE) > 0) {
+						this.onMouseMove(ev)
+					}
+				})
+				bubbleCanvas.addEventListener('wheel', (ev: MouseEvent) => {
+					if ((FLAGS_MODE[this.mode] & FLAG_MOUSE_WHEEL) > 0) {
+						this.onMouseWheel(ev)
+					}
+				})
+				bubbleCanvas.addEventListener('mousedown', (ev: MouseEvent) => {
+					isMouseDown = true
+					if ((FLAGS_MODE[this.mode] & FLAG_MOUSE_DOWN) > 0) {
+						this.onMouseDown(ev)
+					}
 				})
 				bubbleCanvas.addEventListener('mouseup', (ev: MouseEvent) => {
-					this.onMouseUp(ev)
+					isMouseDown = false
+					if ((FLAGS_MODE[this.mode] & FLAG_MOUSE_UP) > 0) {
+						this.onMouseUp(ev)
+					}
 				})
+			} else {
+				alert('Error to fill data.')
 			}
 		})
+	}
+
+	switchMode(mode: Mode) {
+		this.mode = mode
 	}
 
 	fillImageData(): Observable<boolean> {
 		return new Observable<boolean>((observer) => {
 			this.mapService.getAllPoints().subscribe((allPoints) => {
 				if (allPoints != null) {
-					let art = new Array<string>(allPoints.maxLine)
-					let palette = {}
+					this.art = new Array<string>(allPoints.maxLine)
+					this.palette = {}
 					let colorToChar = {}
 					let nextChar = 'A'
 					for (let y = 1; y <= allPoints.maxLine; ++y) {
@@ -69,23 +127,16 @@ export class MapComponent implements OnInit {
 								}
 								colorToChar[point.color] = ch
 								nextChar = String.fromCharCode(ch.charCodeAt(0) + 1)
-								palette[ch] = utils.colorIntToHex(point.color)
+								this.palette[ch] = utils.colorIntToHex(point.color)
 							}
 							artLine += colorToChar[point.color]
 						}
-						art[y - 1] = artLine
+						this.art[y - 1] = artLine
 					}
-
-					var pixel = require('pixel-art');
-					pixel.art(art)
-						.palette(palette)
-						.pos({ x: 0, y: 0 })
-						.scale(this.scale)
-						.draw(this.context);
-					this.logger.info('Pixel art is finished.')
 
 					this.maxLine = allPoints.maxLine
 					this.points = allPoints.points
+					this.drawAllPoints()
 					observer.next(true)
 				} else {
 					observer.next(false)
@@ -94,15 +145,26 @@ export class MapComponent implements OnInit {
 		})
 	}
 
+	drawAllPoints() {
+		this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height)
+		var pixel = require('pixel-art');
+		pixel.art(this.art)
+			.palette(this.palette)
+			.pos({ x: this.totalOffset.x / this.scale, y: this.totalOffset.y / this.scale })
+			.scale(this.scale)
+			.draw(this.context);
+		this.logger.info('Pixel art is drawn with scale ' + this.scale)
+	}
+
 	onMouseMove(ev: MouseEvent) {
 		this.bubble.clearRect(0, 0, this.bubble.canvas.width, this.bubble.canvas.height)
 		let offsetX = ev.offsetX + 10
 		let offsetY = ev.offsetY + 10
-		let x = Math.floor(ev.offsetX / this.scale)
-		let y = Math.floor(ev.offsetY / this.scale)
+		let x = Math.floor((ev.offsetX - this.totalOffset.x) / this.scale)
+		let y = Math.floor((ev.offsetY - this.totalOffset.y) / this.scale)
 
 		let i = y * this.maxLine + x
-		if (i >= 0 && i < this.points.length) {
+		if (x >= 0 && x < this.maxLine && y >=0 && y < this.maxLine && i < this.points.length) {
 			let point = this.points[i]
 			this.bubble.fillStyle = utils.colorIntToHex(point.color)
 			this.bubble.fillRect(offsetX, offsetY, 320, 70)
@@ -117,7 +179,43 @@ export class MapComponent implements OnInit {
 		}
 	}
 
-	onMouseUp(ev: MouseEvent) {
+	onMouseWheel(ev: MouseEvent) {
+		const time = Date.now()
+		if (time - this.lastScaleTime < 200) {
+			return
+		}
+		this.lastScaleTime = time
 
+		this.scale *= ev.wheelDelta > 0 ? 2 : 0.5
+		this.drawAllPoints()
+	}
+
+	onMouseDown(ev: MouseEvent) {
+		this.lastDragXY = {
+			x: ev.offsetX,
+			y: ev.offsetY
+		}
+		this.bubble.clearRect(0, 0, this.bubble.canvas.width, this.bubble.canvas.height)
+	}
+
+	onMouseUp(ev: MouseEvent) {
+		this.bubble.canvas.style.left = this.context.canvas.style.left
+		this.bubble.canvas.style.top = this.context.canvas.style.top
+	}
+
+	onMouseDrag(ev: MouseEvent) {
+		let offsetX = ev.offsetX - this.lastDragXY.x
+		let offsetY = ev.offsetY - this.lastDragXY.y
+		this.lastDragXY = {
+			x: ev.offsetX,
+			y: ev.offsetY
+		}
+		this.totalOffset.x += offsetX
+		this.totalOffset.y += offsetY
+		this.redraw()
+	}
+
+	redraw() {
+		this.drawAllPoints()
 	}
 }
