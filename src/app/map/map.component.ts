9aae9f76fba5ae7d8a4ebee81a55be97335f7863
bluesky139 <bluesky139@gmail.com>
2018-11-28 11:25:38 +0800

    Popup point info at mouse position.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild, ViewContainerRef,  } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
-import { MapService } from '../map.service'
+import { MapService, Point } from '../map.service'
+import { Observable } from 'rxjs';
+import * as utils from '../utils'
 
 @Component({
 	selector: 'app-map',
@@ -11,6 +13,10 @@ import { MapService } from '../map.service'
 export class MapComponent implements OnInit {
 
 	context: CanvasRenderingContext2D
+	bubble: CanvasRenderingContext2D
+	scale: number = 6
+	maxLine: number
+	points: Array<Point>
 
 	constructor(
 		private mapService: MapService,
@@ -22,51 +28,96 @@ export class MapComponent implements OnInit {
 
 	ngAfterViewInit() {
 		const mapCanvasContainer = document.getElementById('mapCanvasContainer')
-		const canvas = document.createElement('canvas')
+		const canvas = document.getElementById('points') as HTMLCanvasElement
 		this.context = canvas.getContext('2d')
 		canvas.width = mapCanvasContainer.clientWidth
 		canvas.height = mapCanvasContainer.clientHeight
-		mapCanvasContainer.appendChild(canvas)
 
-		this.fillImageData()
-		canvas.addEventListener('mousemove', (ev: MouseEvent) => {
-			return this.onMouseMove(ev)
+		const bubbleCanvas = document.getElementById('bubble') as HTMLCanvasElement
+		this.bubble = bubbleCanvas.getContext('2d')
+		bubbleCanvas.width = mapCanvasContainer.clientWidth
+		bubbleCanvas.height = mapCanvasContainer.clientHeight
+
+		this.fillImageData().subscribe((filled) => {
+			if (filled) {
+				bubbleCanvas.addEventListener('mousemove', (ev: MouseEvent) => {
+					this.onMouseMove(ev)
+				})
+				bubbleCanvas.addEventListener('mouseup', (ev: MouseEvent) => {
+					this.onMouseUp(ev)
+				})
+			}
 		})
 	}
 
-	fillImageData() {
-		this.mapService.getAllPoints().subscribe((allPoints) => {
-			let art = new Array<string>(allPoints.maxLine)
-			let palette = {}
-			let colorToChar = {}
-			let nextChar = 'A'
-			for (let y = 1; y <= allPoints.maxLine; ++y) {
-				let artLine = ''
-				for (let x = 1; x <= allPoints.maxLine; ++x) {
-					let point = allPoints.points[(y - 1) * allPoints.maxLine + x - 1]
-					let ch
-					if (!(point.color in colorToChar)) {
-						ch = nextChar
-						colorToChar[point.color] = ch
-						nextChar = String.fromCharCode(ch.charCodeAt(0) + 1)
-						palette[ch] = '#' + ('00000' + point.color.toString(16)).slice(-6)
+	fillImageData(): Observable<boolean> {
+		return new Observable<boolean>((observer) => {
+			this.mapService.getAllPoints().subscribe((allPoints) => {
+				if (allPoints != null) {
+					let art = new Array<string>(allPoints.maxLine)
+					let palette = {}
+					let colorToChar = {}
+					let nextChar = 'A'
+					for (let y = 1; y <= allPoints.maxLine; ++y) {
+						let artLine = ''
+						for (let x = 1; x <= allPoints.maxLine; ++x) {
+							let point = allPoints.points[(y - 1) * allPoints.maxLine + x - 1]
+							if (!(point.color in colorToChar)) {
+								let ch = nextChar
+								if (ch == 'z') {
+									throw new Error('Too many different colors.')
+								}
+								colorToChar[point.color] = ch
+								nextChar = String.fromCharCode(ch.charCodeAt(0) + 1)
+								palette[ch] = utils.colorIntToHex(point.color)
+							}
+							artLine += colorToChar[point.color]
+						}
+						art[y - 1] = artLine
 					}
-					artLine += colorToChar[point.color]
-				}
-				art[y - 1] = artLine
-			}
 
-			var pixel = require('pixel-art');
-			pixel.art(art)
-				.palette(palette)
-				.pos({ x: 0, y: 0 })
-				.scale(6)
-				.draw(this.context);
-			this.logger.info('Pixel art is finished.')
+					var pixel = require('pixel-art');
+					pixel.art(art)
+						.palette(palette)
+						.pos({ x: 0, y: 0 })
+						.scale(this.scale)
+						.draw(this.context);
+					this.logger.info('Pixel art is finished.')
+
+					this.maxLine = allPoints.maxLine
+					this.points = allPoints.points
+					observer.next(true)
+				} else {
+					observer.next(false)
+				}
+			})
 		})
 	}
 
 	onMouseMove(ev: MouseEvent) {
-		//console.log(ev.offsetX, ev.offsetY)
+		this.bubble.clearRect(0, 0, this.bubble.canvas.width, this.bubble.canvas.height)
+		let offsetX = ev.offsetX + 10
+		let offsetY = ev.offsetY + 10
+		let x = Math.floor(ev.offsetX / this.scale)
+		let y = Math.floor(ev.offsetY / this.scale)
+
+		let i = y * this.maxLine + x
+		if (i >= 0 && i < this.points.length) {
+			let point = this.points[i]
+			this.bubble.fillStyle = utils.colorIntToHex(point.color)
+			this.bubble.fillRect(offsetX, offsetY, 320, 70)
+			this.bubble.fillStyle = 'lightgray'
+			this.bubble.lineWidth = 1
+			this.bubble.strokeRect(offsetX, offsetY, 320, 70)
+			this.bubble.fillStyle = 'black'
+			this.bubble.font = '15px Arial'
+			this.bubble.fillText(point.owner == '' ? 'No owner' : point.owner, offsetX + 5, offsetY + 20)
+			this.bubble.fillText(x + ', ' + y, offsetX + 5, offsetY + 40)
+			this.bubble.fillText(point.price + ' ONT to buy', offsetX + 5, offsetY + 60)
+		}
+	}
+
+	onMouseUp(ev: MouseEvent) {
+
 	}
 }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -3,3 +3,7 @@ import { environment } from '../environments/environment'
 export function getURL(path: string): string {
 	return environment.backend.root + path
 }
+
+export function colorIntToHex(color: number): string {
+	return '#' + ('00000' + color.toString(16)).slice(-6)
+}
